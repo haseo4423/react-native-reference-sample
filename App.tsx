19cefd9199a0c0f77a02b5372ed66325d59dbc77
haseo4423 <hunter.4423@gmail.com>
2020-01-23 19:40:39 +0900

    unstatedでステート管理する

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { Provider } from "unstated";
 
 import HomeScreen from './components/HomeScreen';
 import InvoiceEditScreen from './components/InvoiceEditScreen';
 import SummaryScreen from './components/SummaryScreen';
+
+import InvoiceContainer from "./containers/InvoiceContainer";
 
 const RootStack = createStackNavigator(
   {
@@ -21,6 +24,11 @@ const AppContainer = createAppContainer(RootStack);
 
 export default class App extends React.Component {
   render() {
-    return <AppContainer />;
+    let globalState = new InvoiceContainer({ initialSeeding: true });
+    return (
+      <Provider inject={[globalState]}>
+        <AppContainer />
+      </Provider>
+    );
   }
 }
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import { StyleSheet, Button, Text, View } from 'react-native';
-import styles from '../styles.js';
+import { Button, Text, View } from 'react-native';
+import { Subscribe } from "unstated";
 
-class HomeScreen extends React.Component {
+import styles from '../styles.js';
+import InvoiceContainer from "../containers/InvoiceContainer";
+import Invoice from '../models/Invoice.js';
+
+class HomeScreenContent extends React.Component {
     render() {
+        let globalState = this.props.globalState;
+        let invoiceList = <Text>No invoice</Text>;
+        if (globalState.state.data.invoices.length) {
+            invoiceList = globalState.state.data.invoices.map(invoice => {
+                return <Text key={invoice.id}>{invoice.id + " : " + invoice.date}</Text>;
+            });
+        }
+
         return (
             <View style={styles.container}>
                 <Text>Open up App.js to start working on your app!</Text>
                 <Text>Hello World!</Text>
-                <Button
-                    title="Go to InvoiceEdit"
-                    onPress={() => this.props.navigation.navigate('InvoiceEdit')}
-                />
-                <Button
-                    title="Go to Summary"
-                    onPress={() => this.props.navigation.navigate('Summary')}
-                />
+                <Button title="Go to InvoiceEdit" onPress={() => this.props.navigation.navigate('InvoiceEdit')} />
+                <Button title="Go to Summary" onPress={() => this.props.navigation.navigate('Summary')} />
+
+                {invoiceList}
             </View>
         );
     }
+}
+
+const HomeScreen = ({ navigation }) => {
+    return (
+        <Subscribe to={[InvoiceContainer]}>
+            {globalState => <HomeScreenContent globalState={globalState} navigation={navigation} />}
+        </Subscribe>
+    );
 }
 
 export default HomeScreen;
